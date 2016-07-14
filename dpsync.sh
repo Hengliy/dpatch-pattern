25480566c2b287a5fb34d8a5f7f7698353587c25
Wei Yongjun <weiyj.lk@gmail.com>
2016-07-14 08:49:36 +0800

    update pattern sync scripts
    Signed-off-by: Wei Yongjun <weiyj.lk@gmail.com>

--- dpsync.sh
+++ dpsync.sh
@@ -1,4 +1,4 @@
-DIR=/var/lib/dpatch/pattern/cocci/
+DIR=/var/lib/dpatch/pattern/cocci/defined/
 
 if [ ! -d "$DIR" ]; then
 	echo '$DIR does not exists'
@@ -8,46 +8,25 @@ fi
 NPCNT=0
 UPCNT=0
 
-PATTERNS=`cd $DIR/patchs; ls *.cocci`
+PATTERNS=`find $DIR -name *.cocci`
 
 for p in $PATTERNS
 do
-	DST=`find patchs -name $p`
+	bp=`basename $p`
+	DST=`find . -name $bp`
 	if [ "$DST" != "" ]; then
-		diff $DIR/patchs/$p $DST > /dev/null
+		diff $p $DST > /dev/null
 		if [ $? -ne 0 ]; then
-			echo "[P] UPDATE $p"
-			cp -rf $DIR/patchs/$p $DST
+			echo "[P] UPDATE $bp"
+			cp -rf $p $DST
 			UPCNT=$((UPCNT+1))
 		fi
 	else
-			echo "[P] NEW    $p"
-			NPCNT=$((NPCNT+1))
-	fi
-done
-
-NRCNT=0
-URCNT=0
-
-PATTERNS=`cd $DIR/reports; ls *.cocci`
-
-for p in $PATTERNS
-do
-	DST=`find reports -name $p`
-	if [ "$DST" != "" ]; then
-		diff $DIR/reports/$p $DST > /dev/null
-		if [ $? -ne 0 ]; then
-			echo "[R] UPDATE $p"
-			cp -rf $DIR/reports/$p $DST
-			URCNT=$((URCNT+1))
-		fi
-	else
-			echo "[R] NEW    $p"
-			NRCNT=$((NRCNT+1))
+		echo "[P] NEW    $p"
+		NPCNT=$((NPCNT+1))
 	fi
 done
 
 echo
 echo "Total Patch  UPDATE: $UPCNT, NEW: $NPCNT"
-echo "Total Eeport UPDATE: $URCNT, NEW: $NRCNT"
 
--- newpattern.sh
+++ newpattern.sh
@@ -1,6 +1,19 @@
-COCCI=$1
+DIR=/var/lib/dpatch/pattern/cocci/defined/
 
-MSG=`sh cocci2msg.sh $1`
-git add $1
-git commit -s -m "$MSG"
-git log -n 1
+PATTERNS=`find $DIR -name $1`
+COCCI=$2
+
+for name in $PATTERNS
+do
+	bname=`basename $name`
+	DST=`find . -name $bname`
+	if [ "$DST" != "" ]; then
+		echo "SKIP: $name exists: $DST"
+	else
+		cp $name $COCCI/
+		MSG=`sh cocci2msg.sh $COCCI/$name`
+		git add $COCCI/$name
+		git commit -s -m "$MSG"
+		git log -n 1
+	fi
+done
